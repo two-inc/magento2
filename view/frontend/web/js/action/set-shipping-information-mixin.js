fa0ec6640e4085a1ac7e2a201c9d8c858c4b4a62
Bharat Kunwar <bkunwar@two.inc>
2023-11-10 14:37:44 +0000

    style: Format static assets

--- view/frontend/web/js/action/set-shipping-information-mixin.js
+++ view/frontend/web/js/action/set-shipping-information-mixin.js
@@ -4,15 +4,14 @@
  */
 /*jshint browser:true jquery:true*/
 /*global alert*/
-define([
-    'jquery',
-    'mage/utils/wrapper',
-    'Magento_Checkout/js/model/quote'
-], function ($, wrapper, quote) {
+define(['jquery', 'mage/utils/wrapper', 'Magento_Checkout/js/model/quote'], function (
+    $,
+    wrapper,
+    quote
+) {
     'use strict';
 
     return function (setShippingInformationAction) {
-
         return wrapper.wrap(setShippingInformationAction, function (originalAction) {
             var shippingAddress = quote.shippingAddress();
 
@@ -21,15 +20,13 @@ define([
             }
 
             if (shippingAddress.customAttributes !== undefined) {
-                var attribute = shippingAddress.customAttributes.find(
-                    function (element) {
-                        return element.attribute_code === 'two_telephone';
-                    }
-                );
+                var attribute = shippingAddress.customAttributes.find(function (element) {
+                    return element.attribute_code === 'two_telephone';
+                });
             }
 
             shippingAddress['extension_attributes']['two_telephone'] =
-                (attribute !== undefined) ? attribute.value : '';
+                attribute !== undefined ? attribute.value : '';
 
             // pass execution to original action ('Magento_Checkout/js/action/set-shipping-information')
             return originalAction();
